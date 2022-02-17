836723058913e79540f7fda5c8411198f51e92b8
Jules-C <50776493+Jules-C@users.noreply.github.com>
2022-02-17 13:31:50 -0500

    update token methods

--- android/src/main/java/de/einfachhans/firebase/MessagingService.java
+++ android/src/main/java/de/einfachhans/firebase/MessagingService.java
@@ -6,15 +6,18 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MessagingService extends FirebaseMessagingService {
 
+   private static final String TAG = "FirebasePushPlugin";
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "onMessageReceived");
         FirebasePushPlugin.onNewRemoteMessage(remoteMessage);
     }
 
     @Override
-    public void onNewToken(@NonNull String s) {
-        super.onNewToken(s);
-        FirebasePushPlugin.onNewToken(s);
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+      Log.e("Refreshed token:",token);
+        FirebasePushPlugin.onNewToken(token);
     }
 }
